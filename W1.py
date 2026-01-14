0e452bb3ee8ccfe6478bc63e63a7e0be83684e45
IoriKaede <paranoia170214@gmail.com>
2026-01-14 16:44:07 +0100

    initial

---
+++ W1.py
@@ -0,0 +1,6 @@
+#Which concepts are in cluded in parameter(weight and biases) (never number of layers)
+#perception: output can be either 1 or 0 , so 2 different values
+# A neural network is called deep when L > 2 and shallow when L = 2. A neural network is narrow when its widths is in the same order of magnitude as its input size and wide when it has a layer that is significantly larger than the input size
+#weight: 步数 parameters
+#parameter amount: input and 节点 排列组合
+
